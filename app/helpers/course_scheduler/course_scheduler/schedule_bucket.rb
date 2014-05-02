cf2a66c15d22fdba3aff92b425251c45e7ac8dd5
Alex Teske <alex.teske@hotmail.com>
2014-05-01 21:59:18 -0400

    Move the statistics calculation to the bucket class, add the "hours of break" statistic

--- app/helpers/course_scheduler/course_scheduler/schedule_bucket.rb
+++ app/helpers/course_scheduler/course_scheduler/schedule_bucket.rb
@@ -23,4 +23,41 @@ class ScheduleBucket
     end
     description
   end
+
+  def statistics
+    @statistics ||= calculate_statistics
+  end
+
+private
+
+  def calculate_statistics
+    statistics = Hash.new
+
+    #all courses in the bucket have the same statistics
+    course = @schedules.first
+
+    class_per_day = {}
+    hours_of_class = 0
+    hours_of_breaks = 0
+    course.activities.each do |activity|
+      class_per_day[activity.day] ||= Hash.new
+      class_per_day[activity.day][:start_time] = [(class_per_day[activity.day][:start_time] || activity.start_time),activity.start_time].min
+      class_per_day[activity.day][:end_time] = [(class_per_day[activity.day][:end_time] || activity.end_time),activity.end_time].max
+      class_per_day[activity.day][:course_length] ||= 0
+      class_per_day[activity.day][:course_length] += activity.length
+      hours_of_class += activity.length
+    end
+
+    class_per_day.each do |day, hours|
+      total_day_length = ((hours[:end_time] - hours[:start_time]) * 24)
+      hours_of_breaks += total_day_length - hours[:course_length]
+    end
+
+    statistics["Days of class"] = class_per_day.keys.length
+    statistics["Hours of class"] = hours_of_class
+    statistics["Hours of breaks"] = hours_of_breaks
+
+    statistics
+  end
+
 end
